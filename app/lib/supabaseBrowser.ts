8ee4f57b56a78a5c66327b759269952db7174843
mende202 <dariomendelek@tenisclubargentino.com>
2025-09-28 19:57:40 -0300

    Update supabaseBrowser.ts

--- app/lib/supabaseBrowser.ts
+++ app/lib/supabaseBrowser.ts
@@ -1,7 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+// app/lib/supabaseBrowser.ts
+'use client';
 
-export const supabaseBrowser = () =>
-  createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
