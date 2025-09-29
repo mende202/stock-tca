2b26114472d99fb0be3a92c1a2d584db4c292e80
mende202 <dariomendelek@tenisclubargentino.com>
2025-09-29 08:52:36 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Products from './products';
-import { CartProvider } from '../components/CartContext';
-export default async function Page(){
-  return (
-    <CartProvider>
-      <Products />
-    </CartProvider>
-  )
-}
