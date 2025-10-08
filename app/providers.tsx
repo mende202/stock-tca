06855be20d946afb23e90907b6c22045d925f9fb
mende202 <dariomendelek@tenisclubargentino.com>
2025-10-08 00:54:10 -0300

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { CartProvider } from '../components/CartContext';
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return <CartProvider>{children}</CartProvider>;
+}
