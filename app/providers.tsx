74078fc719a5a11b960b123fc237cabad47c29f3
mende202 <dariomendelek@tenisclubargentino.com>
2025-10-08 00:51:54 -0300

    Delete app/providers.tsx

--- app/providers.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { CartProvider } from '../components/CartContext';
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <CartProvider>{children}</CartProvider>; 
-}
